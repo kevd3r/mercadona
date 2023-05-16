75d0db42ec7cc62b57122b11b5ee3da569c92591
kevin derot <kevin.derot@gmail.com>
2023-05-16 08:42:56 +0200

    added css classes and a responsive menu

--- src/Controller/PageControlellerController.php
+++ src/Controller/PageControlellerController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageControlellerController extends AbstractController
 {
-    #[Route('/home', name: 'app_page_controleller')]
+    #[Route('/', name: 'app_page_controleller')]
     public function index(): Response
     {
         return $this->render('page_controleller/index.html.twig', [
